a85e0c2b5c1cf3af48c2370cb93236622741e06c
Yash Khemani <89866628+yashkhemani@users.noreply.github.com>
2025-10-04 17:23:13 +0530

    Added Recpatch

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function beforeSubmit() {
   let formatedDate = new Date(inputDate.value).toLocaleDateString("en-US");
   outputDate.value = formatedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
